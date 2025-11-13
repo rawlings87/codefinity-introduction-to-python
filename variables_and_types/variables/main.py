d4f619f57d69f6098fff08d2c7c64668054c3dd0
rawlings87 <>
2025-11-13 02:58:18 +0000

    Variables

---
+++ variables_and_types/variables/main.py
@@ -0,0 +1,10 @@
+# Storing different types of data in variables
+item_name = "Banana"
+item_price = 0.50
+item_quantity = 100
+in_stock = True
+
+print("Item:", item_name)
+print("Price: $", item_price)
+print("Quantity:", item_quantity)
+print("In stock:", in_stock)
